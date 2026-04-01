c11a6600bac2f0119fec5acf9337a4c3e0452eed
bedo fares <bedo.faruk13@gmail.com>
2026-04-01 17:07:42 +0200

    Integrate Tailwind CSS into the project by importing it in index.css and main.tsx; ensure styles are applied correctly.

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,12 @@
+import "./index.css";
+
 import "primeicons/primeicons.css";
 import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
